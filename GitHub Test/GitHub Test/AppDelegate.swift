a337c519b3f8b6144b4fc06aa0fff6a3109a83d1
Alexander Völz <alexander.voelz@googlemail.com>
2018-03-15 16:26:15 +0100

    first commit - testing git hub connection in team

---
+++ GitHub Test/GitHub Test/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GitHub Test
+//
+//  Created by Alexander Völz on 15.03.18.
+//  Copyright © 2018 Alexander Völz. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ GitHub Test/GitHub Test/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  GitHub Test
+//
+//  Created by Alexander Völz on 15.03.18.
+//  Copyright © 2018 Alexander Völz. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+

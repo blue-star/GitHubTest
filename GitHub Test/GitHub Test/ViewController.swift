4b2a5f9f46e7025ceb2bb7c483e13c8f6c46d14b
Alexander Völz <alexander.voelz@googlemail.com>
2018-03-16 23:17:25 +0100

    cleaned up project

--- GitHub Test/GitHub Test/ViewController.swift
+++ GitHub Test/GitHub Test/ViewController.swift
@@ -28,9 +28,6 @@ class ViewController: NSViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
     }
 }
